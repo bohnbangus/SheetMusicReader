9271fa1b3e2b0eb58de94657330fc7c93a78de08
Drew Edgar <drew.edgar64@gmail.com>
2021-01-15 11:00:38 +0000

    First push

---
+++ SheetMusicReader/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace SheetMusicReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.mainMenu1 = new System.Windows.Forms.MainMenu(this.components);
+            this.menuItemFile = new System.Windows.Forms.MenuItem();
+            this.menuItemOpen = new System.Windows.Forms.MenuItem();
+            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            this.menuHelp = new System.Windows.Forms.MenuItem();
+            this.menuTutorial = new System.Windows.Forms.MenuItem();
+            this.menuAbout = new System.Windows.Forms.MenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.printButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.zoomButton = new System.Windows.Forms.ToolStripButton();
+            this.viewDropDown = new System.Windows.Forms.ToolStripComboBox();
+            this.openFileTool = new System.Windows.Forms.OpenFileDialog();
+            this.printMusic = new System.Windows.Forms.PrintDialog();
+            this.tableImagePanel = new System.Windows.Forms.TableLayoutPanel();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItemFile,
+            this.menuHelp});
+            // 
+            // menuItemFile
+            // 
+            this.menuItemFile.Index = 0;
+            this.menuItemFile.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItemOpen,
+            this.menuItem1});
+            this.menuItemFile.Text = "File";
+            this.menuItemFile.Click += new System.EventHandler(this.menuItem1_Click);
+            // 
+            // menuItemOpen
+            // 
+            this.menuItemOpen.Index = 0;
+            this.menuItemOpen.Text = "Open";
+            this.menuItemOpen.Click += new System.EventHandler(this.menuItem3_Click);
+            // 
+            // menuItem1
+            // 
+            this.menuItem1.Index = 1;
+            this.menuItem1.Text = "Exit";
+            this.menuItem1.Click += new System.EventHandler(this.menuItem1_Click_1);
+            // 
+            // menuHelp
+            // 
+            this.menuHelp.Index = 1;
+            this.menuHelp.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuTutorial,
+            this.menuAbout});
+            this.menuHelp.Text = "Help";
+            // 
+            // menuTutorial
+            // 
+            this.menuTutorial.Index = 0;
+            this.menuTutorial.Text = "Tutorial";
+            // 
+            // menuAbout
+            // 
+            this.menuAbout.Index = 1;
+            this.menuAbout.Text = "About";
+            this.menuAbout.Click += new System.EventHandler(this.menuAbout_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.AutoSize = false;
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(30, 30);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.printButton,
+            this.toolStripSeparator1,
+            this.zoomButton,
+            this.viewDropDown});
+            this.toolStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolStrip1.Size = new System.Drawing.Size(828, 45);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // printButton
+            // 
+            this.printButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.printButton.Image = ((System.Drawing.Image)(resources.GetObject("printButton.Image")));
+            this.printButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.printButton.Name = "printButton";
+            this.printButton.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.printButton.Size = new System.Drawing.Size(54, 42);
+            this.printButton.Text = "toolStripButton1";
+            this.printButton.ToolTipText = "Print";
+            this.printButton.Click += new System.EventHandler(this.printButton_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 45);
+            // 
+            // zoomButton
+            // 
+            this.zoomButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.zoomButton.Image = ((System.Drawing.Image)(resources.GetObject("zoomButton.Image")));
+            this.zoomButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.zoomButton.Name = "zoomButton";
+            this.zoomButton.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.zoomButton.Size = new System.Drawing.Size(44, 42);
+            this.zoomButton.Text = "toolStripButton1";
+            this.zoomButton.ToolTipText = "Zoom";
+            this.zoomButton.Click += new System.EventHandler(this.zoomButton_Click);
+            // 
+            // viewDropDown
+            // 
+            this.viewDropDown.AutoSize = false;
+            this.viewDropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.viewDropDown.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.viewDropDown.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.viewDropDown.Items.AddRange(new object[] {
+            "Side By Side",
+            "Vertical"});
+            this.viewDropDown.Name = "viewDropDown";
+            this.viewDropDown.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.viewDropDown.Size = new System.Drawing.Size(150, 23);
+            this.viewDropDown.DropDownClosed += new System.EventHandler(this.viewDropDown_Change);
+            this.viewDropDown.Click += new System.EventHandler(this.viewDropDown_Click);
+            // 
+            // openFileTool
+            // 
+            this.openFileTool.FileName = "openFileDialog2";
+            // 
+            // printMusic
+            // 
+            this.printMusic.UseEXDialog = true;
+            // 
+            // tableImagePanel
+            // 
+            this.tableImagePanel.AutoScroll = true;
+            this.tableImagePanel.AutoSize = true;
+            this.tableImagePanel.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.tableImagePanel.ColumnCount = 1;
+            this.tableImagePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 46.71623F));
+            this.tableImagePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 53.28377F));
+            this.tableImagePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableImagePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableImagePanel.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.AddColumns;
+            this.tableImagePanel.Location = new System.Drawing.Point(0, 45);
+            this.tableImagePanel.Name = "tableImagePanel";
+            this.tableImagePanel.Padding = new System.Windows.Forms.Padding(15);
+            this.tableImagePanel.RowCount = 1;
+            this.tableImagePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableImagePanel.Size = new System.Drawing.Size(828, 532);
+            this.tableImagePanel.TabIndex = 2;
+            this.tableImagePanel.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(828, 577);
+            this.Controls.Add(this.tableImagePanel);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Menu = this.mainMenu1;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Sheet Music Reader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MainMenu mainMenu1;
+        private System.Windows.Forms.MenuItem menuItemFile;
+        private System.Windows.Forms.MenuItem menuItemOpen;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton printButton;
+        private System.Windows.Forms.ToolStripButton zoomButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.OpenFileDialog openFileTool;
+        private System.Windows.Forms.MenuItem menuHelp;
+        private System.Windows.Forms.MenuItem menuTutorial;
+        private System.Windows.Forms.MenuItem menuAbout;
+        private System.Windows.Forms.PrintDialog printMusic;
+        private System.Windows.Forms.ToolStripComboBox viewDropDown;
+        private System.Windows.Forms.TableLayoutPanel tableImagePanel;
+        private System.Windows.Forms.MenuItem menuItem1;
+    }
+}
+
---
+++ SheetMusicReader/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SheetMusicReader
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Default Form Constructor
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            tableImagePanel.AutoScroll = true;
+        }
+
+        #region Buttons and controls
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            viewDropDown.SelectedItem = "Side By Side"; //Sets the default view of the program
+        }
+
+        private void menuItem6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void menuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void menuItem3_Click(object sender, EventArgs e)
+        {
+            /// <summary>
+            /// The 'Open' button in the menu bar
+            /// Calls the Open File Dialog function, so that the user can open pick a file
+            /// </summary>
+            openFileTool.Filter = "Image Files (*.jpg,*.jpeg,*.png)|*.jpg;*.jpeg;*.png"; //Only allows the user to open image files
+            openFileTool.Multiselect = true;
+            openFileTool.ShowDialog(); //Opens the file picker dialog
+            filepath = openFileTool.FileNames;
+            loadPics(); //Calls the method to refresh the picture box
+        }
+
+        private void openFileDialog1(object sender, CancelEventArgs e)
+        {
+        }
+
+        private void printButton_Click(object sender, EventArgs e) //When print button is clicked
+        {
+/*            PrintDocument pd = new PrintDocument(); //create new instance of PrintDocument
+            if (String.IsNullOrEmpty(filepath)==false) //Check if a file has been selected
+            {
+                pd.PrintPage += new PrintPageEventHandler(PrintPage); //Call PrintPage and add pages to be printed to the PrintDocument
+                PrintPreviewDialog ppv = new PrintPreviewDialog(); //Create new instance of PrintPreview
+                ppv.Document = pd; //Link PrintPreview to the document to be printed
+                ppv.ShowDialog(); //Show the print preview dialog
+
+                PrintDialog pdlg = new PrintDialog(); //Create new instance of the PrintDialog
+                pdlg.Document = pd; //Link PrintDialog to the document
+                DialogResult result = pdlg.ShowDialog(); //Show the print dialog and store whether the user clicks 'OK' or not
+                if (result == DialogResult.OK) //The user clicked 'OK' then print the document 
+                {
+                    pd.Print();
+                }
+            }  
+            else
+            {
+                MessageBox.Show("No file has been selected.", "Error", MessageBoxButtons.OK);
+            }*/
+
+        }
+
+        private void zoomButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //When the about button is clicked
+        private void menuAbout_Click(object sender, EventArgs e)
+        {
+            //A message box pops up with information about the program
+            MessageBox.Show("Take your time.\n\nCreated by Drew Edgar.", "About", MessageBoxButtons.OK);
+        }
+
+        private void viewDropDown_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void viewDropDown_Change(object sender, EventArgs e) //Event fires if the drop down box is opened then closed
+        {
+            if (viewDropDown.Text == "Vertical")
+            {
+                MessageBox.Show("Take your time.\n\nCreated by Drew Edgar.", "About", MessageBoxButtons.OK);
+            }
+        }
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void menuItem1_Click_1(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+        #endregion
+        #region Helper Code
+        public string[] filepath { get; set; }
+        private void loadPics() //This method loads the images into the table container
+        {
+            if (filepath!=null && filepath.Length != 0) //Checks to see if a file has been selected
+            {
+                if (viewDropDown.Text == "Side By Side")
+                {
+
+
+                    tableImagePanel.Controls.Clear(); //Clears table of all controls
+                    tableImagePanel.ColumnStyles.Clear(); //Clears column styles
+                    tableImagePanel.ColumnCount = 0; //Sets number of columns to 1
+                    for (int i = 0; i < filepath.Length; i++) //Loop through all paths in the filepath array
+                    {
+                        tableImagePanel.ColumnCount++; //Adds a column to the table
+                        ColumnStyle cs = new ColumnStyle(); //Creates a new column style
+                        tableImagePanel.ColumnStyles.Add(cs); //Adds new column style to the collection of styles
+                        PictureBox nextPic = new PictureBox(); //Creates a new picture box
+                        tableImagePanel.Controls.Add(nextPic, i, 0); //Places the picture box in the next column
+                        nextPic.Image = Image.FromFile(filepath[i]); //Loads next image into the picture box
+                        nextPic.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right; //Anchors the picture box to all sides of the cell so that it resizes with the window
+                        nextPic.SizeMode = PictureBoxSizeMode.Zoom; //Resizes the image to fit the cell while maintaining the correct aspect ratio/proportions
+                        nextPic.MinimumSize = new Size(300, 300); //Assigns a minimum size to the picture box so that it cannot get to small
+                        cs.SizeType = SizeType.Absolute; //Sets the sizetype of the column to a specific number of pixels
+                        cs.Width = 500; //Assigns a minimum width to the column
+                    }
+                }
+            }
+
+
+        }
+
+
+
+        private void PrintPage(object sender, PrintPageEventArgs e)
+        {
+/*            Image img = Image.FromFile(filepath);
+            Point poi = new Point(100, 100);
+            e.Graphics.DrawImage(img, poi);*/
+        }
+
+
+
+
+
+        #endregion
+
+
+    }
+}
